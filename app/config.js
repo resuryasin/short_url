0662eeff16b72115138efda537692f00dea91f00
resuryasin <672016198@student.uksw.edu>
2022-11-13 23:50:40 +0700

    Remove unused file

--- app/config.js
+++
@@ -1,9 +0,0 @@
-const db_user = process.env.DB_USER;
-const db_pass = process.env.DB_PASS;
-const db_name = process.env.DB_NAME;
-const db_service = process.env.DB_SERVICE;
-
-// expose mongourl to app.js
-module.exports = { 
-    mongoUrl: `mongodb://${db_user}:${db_pass}@${db_service}/${db_name}?authSource=admin`,
-};
