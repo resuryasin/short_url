d3cfd8bab62081ebeb96d93986f48fe57a4e43ee
resuryasin <resuryasin.developer@gmail.com>
2023-08-09 05:52:35 +0000

    Change mongoose strictQuery

--- app/config/db.js
+++ app/config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.connection.once('open', () => {
